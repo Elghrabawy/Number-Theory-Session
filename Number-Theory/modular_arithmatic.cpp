2f7580754ddef1f8f1137958a7d3eee8dc532e20
Ebrahim Elghrbawy <HIMA132004@GMAIL.COM>
2024-11-12 12:45:40 +0200

    edit power

--- Number-Theory/modular_arithmatic.cpp
+++ Number-Theory/modular_arithmatic.cpp
@@ -14,15 +14,14 @@ int modNeg(int a, int m){
     return ((a % m) + m) % m;
 }
 
-int modPow(int a, int n){
+int modPow(int a, int n, int m){
     if(n == 0) return 1; 
     if(n == 1) return a;
 
     if(n % 2 == 0){
-        return power(modMul(a, a), n / 2);
+        return modPow(modMul(a, a, m), n / 2, m);
     }
     else{
-        return modMul(a, power(a, n - 1));
+        return modMul(a, modPow(a, n - 1, m), m);
     }
 }
-
